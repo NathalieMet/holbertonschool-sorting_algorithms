fab6d50d476513592d1792a30151d3b5469891b0
nathalie <nathalie.metreau@hotmail.fr>
2023-09-18 13:01:37 +0200

    Create header file, copy the functions used to print, create bubble_sort function

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * bubble_sort -  sorts an array of integers in ascending order using the
+ * Bubble sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				for (k = 0; k < size; k++)
+				{
+					printf("%d ", array[k]);
+				}
+				printf("\n");
+			}
+		}
+	}
+}
