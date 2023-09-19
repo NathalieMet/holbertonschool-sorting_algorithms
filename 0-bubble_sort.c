60d59631d2c8966a5fe111528a964a912baafc61
letihab <letihabib1822@gmail.com>
2023-09-19 14:23:37 +0200

    rewrite function to bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,29 +1,31 @@
 #include "sort.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * bubble_sort -  sorts an array of integers in ascending order using the
- * Bubble sort algorithm
- * @array: array to sort
+ * bubble_sort - sorts an array of integers in ascending order using
+ * the Bubble sort algorithm
+ * @array: an array of integers
  * @size: size of the array
+ *
+ * Return: void
  */
-
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
+	int swap = 1;
+	int tmp;
+	size_t i;
 
-	for (i = 0; i < size - 1; i++)
+	while (swap)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		swap = 0;
+		for (i = 1; i < size; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i - 1] > array[i])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				tmp = array[i - 1];
+				array[i - 1] = array[i];
+				array[i] = tmp;
+
+				swap = 1;
 				print_array(array, size);
 			}
 		}
