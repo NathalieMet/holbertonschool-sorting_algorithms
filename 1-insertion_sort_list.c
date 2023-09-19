bf46be956768569b61ec1bf40a5530e4ffa434d2
letihab <letihabib1822@gmail.com>
2023-09-19 12:14:38 +0200

    rewrite the function that sort by insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,44 +1,51 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending
- * order using the Insertion sort algorithm
- * @list: list to sort
+ * insertion_sort_list - short description
+ *
+ * Description: long description
+ *
+ * @list: argument_1 description
+ *
+ * Return: return description
  */
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL)
-        return;
+	listint_t *current, *head, *previous, *prev_inner, *current_inner;
 
-    listint_t *current = (*list)->next;
-    
-    while (current != NULL)
-    {
-        listint_t *temp = current->next;
-        listint_t *prev = current->prev;
-        
-        while (prev != NULL && prev->n > current->n)
-        {
-            prev->next = current->next;
-            if (current->next != NULL)
-                current->next->prev = prev;
-            
-            current->prev = prev->prev;
-            current->next = prev;
-            
-            if (prev->prev != NULL)
-                prev->prev->next = current;
-            else
-                *list = current;
-            
-            prev->prev = current;
-            
-            prev = current->prev;
-        }
-        
-        current = temp;
-        
-        print_list(*list);
-    }
+	if (list == NULL)
+		return;
+
+	if ((*list) == NULL || (*list)->next == NULL)
+		return;
+
+	previous = *list;
+	current = (*list)->next;
+	head = *list;
+
+	while (current)
+	{
+		current_inner = current;
+		prev_inner = previous;
+		while (prev_inner && prev_inner->n > current_inner->n)
+		{
+			if (prev_inner->prev == NULL)
+				head = current_inner;
+			if (current_inner == current)
+				current = prev_inner;
+			if (current_inner->next != NULL)
+				current_inner->next->prev = prev_inner;
+			if (prev_inner->prev != NULL)
+				prev_inner->prev->next = current_inner;
+			current_inner->prev = prev_inner->prev;
+			prev_inner->next = current_inner->next;
+			current_inner->next = prev_inner;
+			prev_inner->prev = current_inner;
+			prev_inner = current_inner->prev;
+			print_list(head);
+		}
+		previous = current;
+		current = current->next;
+	}
+	*list = head;
 }
