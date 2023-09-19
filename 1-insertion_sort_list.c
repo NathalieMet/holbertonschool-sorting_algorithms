d3df165716c7914ad4f9a5fdf00d5c24c45c1cab
letihab <letihabib1822@gmail.com>
2023-09-19 11:17:18 +0200

    update file insertion sort list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,53 +1,44 @@
 #include "sort.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
  * insertion_sort_list - sorts a doubly linked list of integers in ascending
- *  order using the Insertion sort algorithm
+ * order using the Insertion sort algorithm
  * @list: list to sort
  */
-
 void insertion_sort_list(listint_t **list)
 {
-	if (list == NULL || *list == NULL)
-		return;
+    if (list == NULL || *list == NULL)
+        return;
 
-	listint_t *head = NULL, *next = NULL;
-	listint_t *actual = *list;
-
-	while (actual != NULL)
-	{
-		 next = actual->next;
-
-        if (head == NULL || actual->n < head->n)
+    listint_t *current = (*list)->next;
+    
+    while (current != NULL)
+    {
+        listint_t *temp = current->next;
+        listint_t *prev = current->prev;
+        
+        while (prev != NULL && prev->n > current->n)
         {
-            actual->next = head;
-            actual->prev = NULL;
-
-            if (head != NULL)
-                head->prev = actual;
-            head = actual;
+            prev->next = current->next;
+            if (current->next != NULL)
+                current->next->prev = prev;
+            
+            current->prev = prev->prev;
+            current->next = prev;
+            
+            if (prev->prev != NULL)
+                prev->prev->next = current;
+            else
+                *list = current;
+            
+            prev->prev = current;
+            
+            prev = current->prev;
         }
-		else
-		{
-            listint_t *temp = head;
-            while (temp->next != NULL && actual->n > temp->next->n)
-                temp = temp->next;
-
-            actual->next = temp->next;
-            actual->prev = temp;
-
-            if (temp->next != NULL)
-                temp->next->prev = actual;
-            temp->next = actual;
-        }
-		if (actual->prev == NULL)
-            *list = actual;
-
-        print_list(head);
-		printf("\n");
-        actual = next;
+        
+        current = temp;
+        
+        print_list(*list);
     }
 }
