25223cbcf7f08781fe37c61b6b2ab3cb635560de
nathalie <nathalie.metreau@hotmail.fr>
2023-09-21 14:41:28 +0200

    add shell sort function

---
+++ 100-shell_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * shell_sort - sorts an array of integers in ascending order using the Shell
+ *  sort algorithm, using the Knuth sequence
+ * @array:pointer on array's adress
+ * @size: size of array
+ * Return: nothing
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t intervalle = 1;
+	int temp;
+	size_t j;
+
+	while (intervalle < size / 3)
+	{
+		intervalle = intervalle * 3 + 1;
+	}
+
+	while (intervalle > 0)
+	{
+		for (size_t i = intervalle; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= intervalle && array[j - intervalle]
+			 > temp; j -= intervalle)
+			{
+				array[j] = array[j - intervalle];
+			}
+			array[j] = temp;
+		}
+
+		intervalle /= 3;
+
+		for (size_t i = 0; i < size; i++)
+		{
+			if (i > 0)
+			printf(", ");
+			printf("%d", array[i]);
+		}
+		printf("\n");
+	}
+}
--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ void swap(int *a, int *b);
 int partition(int *array, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
 void quicksort(int *array, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif /* SORT_H */
